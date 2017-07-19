6a0320660944eba3b7692fe66999c783eaa4cc39
Thomas Carmichael <carmanaught@gmail.com>
2017-07-19 13:53:28 +0800

    Update README, scripts and config
    The old context menu has been heavily rewritten so scripts/configuration have been added and/or changed to accomodate.

---
+++ .config/mpv/scripts/mpvcontextmenu.tcl
@@ -0,0 +1,218 @@
+# #############################################################
+# Context menu constructed via CLI args.
+# Originally by Avi Halachmi (:avih) https://github.com/avih
+# Extended by Thomas Carmichael (carmanught) https://github.com/carmanaught
+#
+# Developed for and used in conjunction with mpvcontextmenu.lua - context-menu for mpv.
+# See mpvcontextmenu.lua for more info.
+#
+# 2017-02-02 - Version 0.1 - Initial version (avih)
+# 2017-07-19 - Version 0.2 - Extensive rewrite (carmanught)
+# #############################################################
+
+# Required when launching via tclsh, no-op when launching via wish
+package require Tk
+package require Ttk
+font create defFont -family {Source Code Pro} -size 9
+option add *font defFont
+ttk::style theme use clam
+
+# Remove the main window from the host window manager
+wm withdraw .
+
+# I'll leave this code here until I know the argList works properly
+#set argFile [open [lindex $argv 0]]
+#set argList [split [read $argFile] "|"]
+#close $argFile; # Saves a few bytes :-)
+
+set argList [split [lindex $argv 0] "|"]
+
+if { $::argc < 1 } {
+    puts "Usage: context.tcl menufile"
+    exit 1
+}
+
+# Construct the menu from argv:
+# - The first set of values contains the absolute x, y menu position, or
+# - under the mouse if -1, -1, as well as the base menu name.
+# - The rest of the pairs are display-string, return-value-on-click.
+#   If the return value is empty then the display item is disabled, but if the
+#   display is "-" (and empty rv) then a separator is added instead of an item.
+# - For now, return-value is expected to be a number, and -1 is reserved for cancel.
+#
+# On item-click/menu-dismissed, we print a json object to stdout with the
+# menu name and and index.
+set RESP_CANCEL -1
+
+set boxCheck "\[X\] "
+set boxUncheck "\[ \] "
+set radioSelect "(x) "
+set radioEmpty "( ) "
+set boxA "\[A\] "
+set boxB "\[B\] "
+set emptyPre "    "
+set accelSpacer "   "
+set labelPre ""
+set menuWidth 36
+set baseMenuName "context_menu"
+set first 1
+
+# I haven't found a way to right-justify the accelerator so we'll just build the label
+# and shortcut together and add spaces between instead of using:
+# -accel $accelSpacer$itemAccel
+proc makeLabel {pre lbl acl} {
+    set spacesCount [expr $::menuWidth - [string length $pre] - [string length $lbl] - [string length $acl]]
+    set whiteSpace [string repeat " " $spacesCount]
+    set fullLabel $pre$lbl$whiteSpace$acl
+    return $fullLabel
+}
+
+foreach {tableName tableIndex itemType itemLabel itemAccel itemState itemDisable} $argList {
+    if {$first} {
+        set pos_x $tableName
+        set pos_y $tableIndex
+        set baseMenuName $itemType
+        set baseMenu [menu .$baseMenuName -tearoff 0]
+        set curMenu .$itemType
+        set preMenu .$itemType
+        set first 0
+        continue
+    }
+    
+    if {$itemDisable == "false"} {
+        set itemDisable "normal"
+    } elseif {$itemDisable == "true"} {
+        set itemDisable "disabled"
+    } else {
+        set itemDisable "normal"
+    }
+    
+    if {$itemType == "changemenu"} {
+        if {$itemAccel == ""} {
+            # Need to understand how menus work to fix window name $menuname already exists
+            if {![winfo exists .$itemLabel]} {
+                menu .$itemLabel -tearoff 0
+            }
+            set curMenu .$itemLabel
+            set preMenu .$itemLabel
+        } else {
+            if {$itemState == ""} {
+                if {![winfo exists .$itemLabel]} { 
+                    menu .$itemLabel -tearoff 0
+                }
+                if {![winfo exists .$itemLabel.$itemAccel]} {
+                    menu .$itemLabel.$itemAccel -tearoff 0
+                }
+                set curMenu .$itemLabel.$itemAccel
+                set preMenu .$itemLabel
+            } else {
+                if {![winfo exists .$itemLabel]} {
+                    menu .$itemLabel -tearoff 0
+                }
+                if {![winfo exists .$itemLabel.$itemAccel]} {
+                    menu .$itemLabel.$itemAccel -tearoff 0
+                }
+                if {![winfo exists .$itemLabel.$itemAccel.$itemState]} {
+                    menu .$itemLabel.$itemAccel.$itemState -tearoff 0
+                }
+                set curMenu .$itemLabel.$itemAccel.$itemState
+                set preMenu .$itemLabel.$itemAccel
+            }
+        }
+        continue
+    }
+
+    if {$tableName == "cascade"} {
+        # Reverse the $curMenu and $preMenu here so that the menu so that it attaches in the
+        # correct order.
+        $preMenu add cascade -label $emptyPre$tableIndex -state $itemDisable -menu $curMenu
+        continue
+    }
+    
+    if {$itemType == "separator"} {
+        $curMenu add separator
+        continue
+    }
+    
+    if {$itemType == "command"} {
+        $curMenu add command -label [makeLabel $emptyPre $itemLabel $itemAccel] -state $itemDisable -command "done $tableName $tableIndex"
+        continue
+    }
+    
+    # For checkbutton/radiobutton I'll just add command items for now, until I can get the
+    # theming to work right and show the buttons based on a specified theme.
+    
+    if {$itemType == "checkbutton"} {
+        if {$itemState == "true"} {
+            set labelPre $boxCheck
+        } else {
+            set labelPre $boxUncheck
+        }
+        
+        $curMenu add command -label [makeLabel $labelPre $itemLabel $itemAccel] -state $itemDisable -command "done $tableName $tableIndex"
+        continue
+    }
+    
+    if {$itemType == "radiobutton"} {
+        if {$itemState == "true"} {
+            set labelPre $radioSelect
+        } else {
+            set labelPre $radioEmpty
+        }
+        
+        $curMenu add command -label [makeLabel $labelPre $itemLabel $itemAccel] -state $itemDisable -command "done $tableName $tableIndex"
+        continue
+    }
+    
+    if {$itemType == "ab-button"} {
+        if {$itemState == "a"} {
+            set labelPre $boxA
+        } elseif {$itemState == "b"} {
+            set labelPre $boxB
+        } elseif {$itemState == "off"} {
+            set labelPre $boxUncheck
+        }
+        
+        $curMenu add command -label [makeLabel $labelPre $itemLabel $itemAccel] -state $itemDisable -command "done $tableName $tableIndex"
+        continue
+    }
+    
+}
+
+# Read the absolute mouse pointer position if we're not given a pos via argv
+if {$pos_x == -1 && $pos_y == -1} {
+    set pos_x [winfo pointerx .]
+    set pos_y [winfo pointery .]
+}
+
+proc done {menuName index} {
+    puts -nonewline "{\"menuname\":\"$menuName\", \"index\":\"$index\"}"
+    exit
+}
+
+# Seemingly, on both windows and linux, "cancelled" is reached after the click but
+# before the menu command is executed and _a_sync to it. Therefore we wait a bit to
+# allow the menu command to execute first (and exit), and if it didn't, we exit here.
+proc cancelled {} {
+    after 100 {done $baseMenuName $::RESP_CANCEL}
+}
+
+# Calculate the menu position relative to the Tk window
+set win_x [expr {$pos_x - [winfo rootx .]}]
+set win_y [expr {$pos_y - [winfo rooty .]}]
+
+# Launch the popup menu
+tk_popup $baseMenu $win_x $win_y
+
+# On Windows tk_popup is synchronous and so we exit when it closes, but on Linux
+# it's async and so we need to bind to the <Unmap> event (<Destroyed> or
+# <FocusOut> don't work as expected, e.g. when clicking elsewhere even if the
+# popup disappears. <Leave> works but it's an unexpected behavior for a menu).
+# Note: if we don't catch the right event, we'd have a zombie process since no
+#       window. Equally important - the script will not exit.
+# Note: untested on macOS (macports' tk requires xorg. meh).
+if {$tcl_platform(platform) == "windows"} {
+    cancelled
+} else {
+    bind $baseMenu <Unmap> cancelled
+}
--- README.md
+++ README.md
@@ -10,9 +10,7 @@ Possible configurations of interest:
 
 The shorcuts specified in the input.conf are modeled off of what I used in Bomi before moving to straight mpv and the scripts are saved from various gists or github repos, linked from the [mpv scripts page](https://github.com/mpv-player/mpv/wiki/User-Scripts).
 
-One of the things I did miss from Bomi and have sort-of regained, is a right-click menu. Specifically, the [Tcl/Tk context menu](https://gist.github.com/avih/bee746200b5712220b8bd2f230e535de) context.lua and context.tcl files which are used to create a right-click menu. The context.lua specifically has been fairly heavily modified to match the layout of the Bomi menu. Note however that this is a **work-in-progress** and I'll probably remove items that are too difficult or that I just straight don't feel like trying to implement. There may also be bugs.
-
-Also keep in mind that the menu doesn't support proper submenus and I haven't figured out the logistics of how to do this between the LUA and TCL/TK code. Another thing to note is that the context menu uses some of the mpv scripts I've downloaded, such as [subit](https://github.com/wiiaboo/mpv-scripts/blob/master/subit.lua), [playlistmanager](https://github.com/donmaiq/Mpv-Playlistmanager), [filenavigator](https://github.com/donmaiq/mpv-filenavigator) or [stats](https://github.com/Argon-/mpv-stats/).
+I have a seperate github repo for my [mpvcontextmenu script](https://github.com/carmanaught/mpvcontextmenu).
 
 For config in a couple of the scripts, they reference a file externally that I used so as not to have to edit out personal paths in each file each time I want to push updates to this git repo. The personal-values.lua file in this repo also simply contains the default values. I mention this here as if you want to change those values you either need to update the personal-values.lua file or update the scripts yourself.
 
